a50750e87943bd5b81c99a1be4fe8c0fee01e53f
STExSpower <rashedbhai672@gmail.com>
2025-06-19 03:35:06 +0600

    Update stream.ts

--- api/stream.ts
+++ api/stream.ts
@@ -1,1 +1,26 @@
+import { spawn } from 'child_process';
 
+export default function handler(req, res) {
+  const url = req.query.url;
+  if (!url) return res.status(400).send("Missing URL");
+
+  res.setHeader('Content-Type', 'application/vnd.apple.mpegurl');
+
+  const ffmpeg = spawn('ffmpeg', [
+    '-i', url,
+    '-map', '0:v',
+    '-map', '0:a',
+    '-map', '0:s?',
+    '-c:v', 'libx264',
+    '-c:a', 'aac',
+    '-c:s', 'mov_text',
+    '-f', 'hls',
+    '-hls_time', '4',
+    '-hls_list_size', '0',
+    'pipe:1'
+  ]);
+
+  ffmpeg.stdout.pipe(res);
+  ffmpeg.stderr.on('data', data => console.error(`stderr: ${data}`));
+  ffmpeg.on('close', () => res.end());
+}
